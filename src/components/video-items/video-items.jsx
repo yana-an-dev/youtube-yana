5d405399b28cff471e35ea2e91a5ade5a39909e7
Yana <ayh916@gmail.com>
2021-09-29 07:02:30 +1000

    grid of videolist and video items

--- src/components/video-items/video-items.jsx
+++ src/components/video-items/video-items.jsx
@@ -4,8 +4,8 @@ import styles from './video-items.module.css'
 const VideoItems = ({ video: { snippet } }) => (
     <li className={styles.videoitem}>
         <img src={snippet.thumbnails.medium.url} alt="thumbnails"></img>
-        <h3>{snippet.title}</h3>
-        <p>{snippet.channelTitle}</p>
+        <h3 className={styles.title}>{snippet.title}</h3>
+        <p className={styles.channeltitle}>{snippet.channelTitle}</p>
 
     </li>
 );
